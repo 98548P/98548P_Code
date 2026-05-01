34ab0cb2139eeadf37e1ad4b4c859b83e1603147
98548P <colsongibby@gmail.com>
2026-04-30 21:39:51 -0600

    same

--- TheProblemCode-2026-04-23T23-35-41/src/UI.cpp
+++ TheProblemCode-2026-04-23T23-35-41/src/UI.cpp
@@ -36,8 +36,9 @@ void enterScreen(float screenNumber) {
   } else if (screenNumber == 2) {
 
     //odom screen
+    bool good = true;
 
-    while(screenNumber == 2){
+    while(good == true){
 
       //converts doubles to char with 2 decimal places (badly)
 
@@ -47,12 +48,23 @@ void enterScreen(float screenNumber) {
 
       Brain.Screen.clearScreen();
 
-      drawButton(20, 20, 90, 40, blue, white, 2, c, 3, mono12); 
+      if ((drawButton(0, 200, 60, 40, orange, white, 0, "back", 3, mono12) == 1)) {
+        good = false;
+        screen = 0;
+      }
 
+      drawButton(50, 20, 90, 40, black, white, 2, c, 3, mono20); 
+      drawButton(20, 20, 40, 40, black, white, 2, "X:", 3, mono20);
       snprintf(buffer, 50, "%.2f", robotY);
       const char* d = buffer;
 
-      drawButton(20, 83, 90, 40, blue, white, 2, d, 3, mono12); 
+      drawButton(50, 83, 90, 40, black, white, 2, d, 3, mono20); 
+      drawButton(20, 83, 40, 40, black, white, 2, "Y:", 3, mono20);
+      snprintf(buffer, 50, "%.2f", InertialSensor.heading());
+      const char* e = buffer;
+
+      drawButton(115, 146, 90, 40, black, white, 2, e, 3, mono20); 
+      drawButton(20, 146, 94, 40, black, white, 2, "Heading:", 3, mono20);
 
       int gridSize = 6;
       int squareSize = 35;
@@ -78,11 +90,40 @@ void enterScreen(float screenNumber) {
       // draw robot
       Brain.Screen.setFillColor(red);
       Brain.Screen.drawCircle(pixelX, pixelY, 10);
+
+    // Heading arrow
+double headingDeg = InertialSensor.heading();
+double headingRad = ((headingDeg) * (M_PI / 180.0));
+
+double arrowLength = 30;
+
+double arrowX = pixelX + arrowLength * sin(headingRad);
+double arrowY = pixelY - arrowLength * cos(headingRad);
+
+Brain.Screen.setPenColor(green);
+Brain.Screen.setPenWidth(4);
+Brain.Screen.drawLine(pixelX, pixelY, arrowX, arrowY);
+
+
+double angleOffset = 45 * (M_PI / 180.0);
+double headLength = 10;
+
+double leftX = arrowX - headLength * sin(headingRad - angleOffset);
+double leftY = arrowY + headLength * cos(headingRad - angleOffset);
+
+double rightX = arrowX - headLength * sin(headingRad + angleOffset);
+double rightY = arrowY + headLength * cos(headingRad + angleOffset);
+
+Brain.Screen.drawLine(arrowX, arrowY, leftX, leftY);
+Brain.Screen.drawLine(arrowX, arrowY, rightX, rightY);
+
+ 
+
       Brain.Screen.render();
       wait(15, msec);
       }
+  Brain.Screen.renderDisable();  
   }
-  
   if(screenNumber != screen) {
     //this checks if a button was pressed, when the screen is redrawn
     //the screen is redrawn whenenever the brain is pressed, regardless of finger placement
@@ -91,7 +132,7 @@ void enterScreen(float screenNumber) {
   }
 }
 
-void drawButton (int x, int y, int width, int height, color fillColor, color outlineColor, float screenNumber, const char *text, int thickness, fontType font) {
+int drawButton (int x, int y, int width, int height, color fillColor, color outlineColor, float screenNumber, const char *text, int thickness, fontType font) {
   Brain.Screen.setFillColor(fillColor);
   Brain.Screen.setPenColor(outlineColor);
   Brain.Screen.setPenWidth(thickness);
@@ -99,7 +140,7 @@ void drawButton (int x, int y, int width, int height, color fillColor, color out
   Brain.Screen.setPenColor(white);
   Brain.Screen.setFont(font);
 
-  //centers the text inside fo the buttons
+  //centers the text inside of the buttons
 
   int textWidth = Brain.Screen.getStringWidth(text);
   int textHeight = Brain.Screen.getStringHeight(text);
@@ -113,21 +154,24 @@ void drawButton (int x, int y, int width, int height, color fillColor, color out
   if(Brain.Screen.pressing() == true && Brain.Screen.xPosition() > x && Brain.Screen.yPosition() > y && Brain.Screen.xPosition() < (x + width) && Brain.Screen.yPosition() < (y + height)) {
     waitUntil(Brain.Screen.pressing() == false);
     screen = screenNumber;
+    return 1;
   }
+  return 0;
 }
 
-
 const char* defSplash() {
-
+//const char* defSplash() {
+int wow = 0;
   //splash text
   
   const char* defaultSplash[] = {
-    "Also try Minecraft!","Runs on code and bad decisions","CODE333","This side up","Field-Centric feelings","Now with thirty-seven percent more torque!","No keyboard detected, Press F1 to continue","Odom says we're fine","Sponsored by zip ties","Live, Laugh, Localize","Battery low, Confidence high","Rotation sensors my beloved","Precision-ish","Now with emotional stability","Born to intake, forced to defend","Geometry but dangerous","Wheels spinning thoughtfully","Surprisingly reversible","PID and chill","Object permanence enabled","Coded with dramatic intensity","Mildly sentient","Brakes are a mindset","Slightly ahead of schedule","sudo win_elims","ping too high for excuses","Consistently bad","PINNING","Probably within several inches","Do NOT trust the process","Chaos, but organised","I meant to do that","Too tired to be mysterious","The cake is a lie","The inner machinations of my mind are an enigma","We do what we must because we can","We scare because we care","what","Did well...enough","That's what  -She","Notebook about the code","Blame it on Dawn","Wario Style","We tested this. Once"
+    "Also try Minecraft!","Runs on code and bad decisions","CODE333","This side up","Field-Centric feelings","Now with thirty-seven percent more torque!","No keyboard detected, Press F1 to continue","Odom says we're fine","Sponsored by zip ties","Live, Laugh, Localize","Battery low, Confidence high","Rotation sensors my beloved","Precision-ish","Now with emotional stability","Born to intake, forced to defend","Geometry but dangerous","Wheels spinning thoughtfully","Surprisingly reversible","PID and chill","Object permanence enabled","Coded with dramatic intensity","Mildly sentient","Brakes are a mindset","Slightly ahead of schedule","sudo win_elims","ping too high for excuses","Consistently bad","PINNING","Probably within several inches","Do NOT trust the process","Chaos, but organised","I meant to do that","Too tired to be mysterious","The cake is a lie","The inner machinations of my mind are an enigma","We do what we must because we can","We scare because we care","what","Did well...enough","That's what  -She","Notebook about the code","Blame it on Dawn","Wario Style","We tested this. Once","Am I a bucket?","Air pressure now at 200 PSI"
   };
   if(randint == false) {
     randint = true;
-   // randomInt = rand()%44;
-    randomInt = rand() % ((sizeof(defaultSplash)) / (sizeof(defaultSplash[0])));
+    wow = sizeof(defaultSplash);
+    wow = wow/4;
+    randomInt = rand() % wow;
   }
   const char* splash = defaultSplash[randomInt];
   return splash;
